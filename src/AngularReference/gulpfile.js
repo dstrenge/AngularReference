dd5f9c1742242b8b728c911c170114eb19ca4414
kinaxis\dstrenge <dstrenge@kinaxis.com>
2016-05-10 16:11:52 -0400

    initial commit

---
+++ src/AngularReference/gulpfile.js
@@ -0,0 +1,19 @@
+﻿var gulp = require("gulp");
+var fs = require("fs");
+
+var paths = {
+	npm: './node_modules/',
+	lib: "./wwwroot/lib/"
+};
+
+gulp.task("copy", function () {
+	var npm = {
+		"requirejs": "requirejs/require.js",
+		"angular": "angular/angular.js"
+	}
+
+	for (var destinationDir in npm) {
+		console.log(paths.lib + destinationDir);
+		gulp.src(paths.npm + npm[destinationDir]).pipe(gulp.dest(paths.lib + destinationDir));
+	}
+});
---
+++ src/AngularReference/scripts/app/app.module.ts
@@ -0,0 +1,6 @@
+﻿/// <reference path="../../typings/tsd.d.ts" />
+/// <amd-dependency path="angular" />
+
+export var ApplicationClass: ng.IModule = angular.module("vo.application", []).run([(): void => {
+    alert("hey girl!!");
+}]);
